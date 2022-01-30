32305895363e308ce6928607bb51e4d09fa033c7
AlexTailor <szabbo@gmail.com>
2022-01-30 16:16:49 +0100

    update MongoDB connection

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
+const { MongoClient } = require("mongodb");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
-
 const app = express();
 
-//DB Config
-const db = require("./config/keys").mongoURI;
+//DB config
+const uri = require("./config/keys").mongoURI;
 
-//Connect to MongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+// Connect to MongoDB
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => res.send("Hello world"));
 
-//Use Routes
-
+// Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
